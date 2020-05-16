dd447c5b8bfc680ce8ff2d974761cf3b479ff34f
Dai <let__it__be@hb.tp1.jp>
2020-05-17 00:36:23 +0900

    signin+signupエラー回避

--- src/auth/Signin.js
+++ src/auth/Signin.js
@@ -5,13 +5,12 @@ import { AuthContext } from './AuthProvider';
 import GoogleAuth from './GoogleAuth';
 import TwitterAuth from './TwitterAuth';
 import FacebookAuth from './FacebookAuth';
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
 
 const Signin = ({ history }) => {
     const { signin } = useContext(AuthContext);
-    const { register, handleSubmit, errors } = useForm();
 
-    const onSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         const { email, password } = e.target.elements;
         signin(email.value, password.value, history);
@@ -21,33 +20,18 @@ const Signin = ({ history }) => {
         <React.Fragment>
             <h2>Sign in</h2>
             <Link to="/signup">Sign up</Link>
-            <form onSubmit={ handleSubmit(onSubmit) }>
+            <form onSubmit={ handleSubmit }>
                 <input
                     name="email"
                     type="email"
                     placeholder="Email"
-                    ref={register({
-                        required: "Emailは必須項目です",
-                        pattern: {
-                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                          message: "invalid email address"
-                        }
-                    })}
                 />
-                {errors.email && errors.email.message}
 
                 <input
                     name="password"
                     type="password"
                     placeholder="Password"
-                    ref={register({
-                        required: "Passwordは必須項目です",
-                        pattern: {
-                          message: "invalid password"
-                        }
-                    })}
                 />
-                {errors.password && errors.password.message}
 
                 <button type="submit">Sign in</button>
             </form>
@@ -57,5 +41,57 @@ const Signin = ({ history }) => {
         </React.Fragment>
     );
 }
+
+// react-hook-formは動くが登録時にエラーとなる
+// const Signin = ({ history }) => {
+//     const { signin } = useContext(AuthContext);
+//     const { register, handleSubmit, errors } = useForm();
+
+//     const onSubmit = async (e) => {
+//         e.preventDefault();
+//         const { email, password } = e.target.elements;
+//         signin(email.value, password.value, history);
+//     }
+
+//     return (
+//         <React.Fragment>
+//             <h2>Sign in</h2>
+//             <Link to="/signup">Sign up</Link>
+//             <form onSubmit={ handleSubmit(onSubmit) }>
+//                 <input
+//                     name="email"
+//                     type="email"
+//                     placeholder="Email"
+//                     ref={register({
+//                         required: "Emailは必須項目です",
+//                         pattern: {
+//                           value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+//                           message: "invalid email address"
+//                         }
+//                     })}
+//                 />
+//                 {errors.email && errors.email.message}
+
+//                 <input
+//                     name="password"
+//                     type="password"
+//                     placeholder="Password"
+//                     ref={register({
+//                         required: "Passwordは必須項目です",
+//                         pattern: {
+//                           message: "invalid password"
+//                         }
+//                     })}
+//                 />
+//                 {errors.password && errors.password.message}
+
+//                 <button type="submit">Sign in</button>
+//             </form>
+//             <button onClick={ GoogleAuth }>Google認証</button>
+//             <button onClick={ TwitterAuth }>Twitter認証</button>
+//             <button onClick={ FacebookAuth }>Facebook認証(検証中)</button>
+//         </React.Fragment>
+//     );
+// }
 
 export default withRouter(Signin);
--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -2,14 +2,12 @@ import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
 import { AuthContext } from './AuthProvider';
-import { useForm } from 'react-hook-form';
+// import { useForm } from 'react-hook-form';
 
 const Signup = ({ history }) => {
     const { signup } = useContext(AuthContext);
-    const { register, handleSubmit, errors } = useForm();
 
-
-    const onSubmit = (e) => {
+    const handleSubmit = (e) => {
         e.preventDefault();
         const { email, password } = e.target.elements;
         signup(email.value, password.value, history);
@@ -19,39 +17,75 @@ const Signup = ({ history }) => {
         <React.Fragment>
             <h2>Sign up</h2>
             <Link to="/">Sign in</Link>
-            <form onSubmit={ handleSubmit(onSubmit) }>
+            <form onSubmit={ handleSubmit }>
                 
                 <input
                     name="email"
                     type="email"
                     placeholder="Email"
-                    ref={register({
-                        required: "Emailは必須項目です",
-                        pattern: {
-                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                          message: "invalid email address"
-                        }
-                    })}
                 />
-                {errors.email && errors.email.message}
 
                 <input
                     name="password"
                     type="password"
                     placeholder="Password"
-                    ref={register({
-                        required: "Passwordは必須項目です",
-                        pattern: {
-                          message: "invalid password"
-                        }
-                    })}
                 />
-                {errors.password && errors.password.message}
 
                 <button type="submit">Sign in</button>
             </form>
         </React.Fragment>
     );
 }
+
+// react-hook-formは動くが登録時にエラーとなる
+// const Signup = ({ history }) => {
+//     const { signup } = useContext(AuthContext);
+//     const { register, handleSubmit, errors } = useForm();
+
+
+//     const onSubmit = (e) => {
+//         e.preventDefault();
+//         const { email, password } = e.target.elements;
+//         signup(email.value, password.value, history);
+//     }
+
+//     return (
+//         <React.Fragment>
+//             <h2>Sign up</h2>
+//             <Link to="/">Sign in</Link>
+//             <form onSubmit={ handleSubmit(onSubmit) }>
+                
+//                 <input
+//                     name="email"
+//                     type="email"
+//                     placeholder="Email"
+//                     ref={register({
+//                         required: "Emailは必須項目です",
+//                         pattern: {
+//                           value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+//                           message: "invalid email address"
+//                         }
+//                     })}
+//                 />
+//                 {errors.email && errors.email.message}
+
+//                 <input
+//                     name="password"
+//                     type="password"
+//                     placeholder="Password"
+//                     ref={register({
+//                         required: "Passwordは必須項目です",
+//                         pattern: {
+//                           message: "invalid password"
+//                         }
+//                     })}
+//                 />
+//                 {errors.password && errors.password.message}
+
+//                 <button type="submit">Sign in</button>
+//             </form>
+//         </React.Fragment>
+//     );
+// }
 
 export default withRouter(Signup);
